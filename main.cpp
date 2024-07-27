e5bdddbaee44aef927bae621e5d733325110da0e
OmPrakash2k03 <omprakashmahato.it.aec@gmail.com>
2024-07-27 23:45:24 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,29 +4,21 @@ using namespace std;
 
 int main()
 {
-    int marks;
-    cout<<"Enter Your Marks : "<<endl;
-    cin>>marks;
-    if(marks<=40){
-        cout<<"Fail"<<endl;
+    int x,y,z;
+    cout<<"Enter the first sides of Triangle :"<<endl;
+    cin>>x;
+    cout<<"Enter the second sides of Triangle :"<<endl;
+    cin>>y;
+    cout<<"Enter the third sides of Triangle :"<<endl;
+    cin>>z;
+    if(x==y&&y==z&&z==x){
+        cout<<"The given Triangle is Equilateral Triangle."<<endl;
     }
-    else if(marks>40 && marks<=50){
-        cout<<"D"<<endl;
+    else if(x==z||z==x||y==x||y==z){
+        cout<<"The given triangle is Isoscales Triangle."<<endl;
     }
-    else if(marks>50 && marks<=60){
-        cout<<"C"<<endl;
-    }
-    else if(marks>60 && marks<=70){
-        cout<<"B"<<endl;
-    }
-    else if(marks>70 && marks<=80){
-        cout<<"A"<<endl;
-    }
-    else if(marks>80 && marks<=90){
-        cout<<"E"<<endl;
-    }
-    else if(marks>90 && marks<=100){
-        cout<<"O"<<endl;
+    else if(x>=y||y>=z||z>=x){
+        cout<<"The given triangle is Scalene Triangle."<<endl;
     }
     return 0;
 }
